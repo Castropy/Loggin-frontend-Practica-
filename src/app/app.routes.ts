b04565c5df999e34ab0f8da5721587e4f05e5599
Castropy <randrescastroi@gmail.com>
2026-01-05 21:53:33 -0400

    Definicion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LogginComponent } from './loggin/loggin.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LogginComponent },       // ruta por defecto
+  { path: 'registro', component: RegistroComponent } // ruta para registro
+];
