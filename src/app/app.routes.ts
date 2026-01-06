cb78d4d4a2a2de249a2e9a875685a991e3bc6896
Castropy <randrescastroi@gmail.com>
2026-01-06 11:29:05 -0400

    Agregando componente homepage a la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { Homepage } from './homepage/homepage.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },       // ruta por defecto
-  { path: 'registro', component: RegistroComponent } // ruta para registro
+  { path: 'registro', component: RegistroComponent }, // ruta para registro
+  { path: 'homepage', component: Homepage } // ruta para homepage
 ];
