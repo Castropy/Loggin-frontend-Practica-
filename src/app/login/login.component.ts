3b5b530ab0b136e1848f45318b201df3349263ce
Castropy <randrescastroi@gmail.com>
2026-01-06 11:21:41 -0400

    Enviar datos al backend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';   
+import { RouterModule, Router } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-loggin',
+  selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],                        
-  templateUrl: './loggin.component.html',
-  styleUrls: ['./loggin.component.css']
+  imports: [RouterModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class LogginComponent {}
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    // Inicialización dentro del constructor
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ingresar() {
+    if (this.form.valid) {
+      this.http.post('http://localhost:8000/authapp/login/', this.form.value)
+        .subscribe({
+          next: (res: any) => {
+            // Guardar tokens JWT
+            localStorage.setItem('access_token', res.access);
+            localStorage.setItem('refresh_token', res.refresh);
+
+            alert('Ingreso exitoso');
+            this.router.navigate(['/homepage']);
+          },
+          error: () => {
+            alert('Usuario o contraseña incorrecta');
+          }
+        });
+    }
+  }
+}
