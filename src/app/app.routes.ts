3f59bdabb069240e38ccaff68f3f67377c1f6e17
Castropy <randrescastroi@gmail.com>
2026-01-06 11:26:49 -0400

    Correcion del rename loggin a login en la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LogginComponent } from './login/login.component';
+import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 
 export const routes: Routes = [
-  { path: '', component: LogginComponent },       // ruta por defecto
+  { path: '', component: LoginComponent },       // ruta por defecto
   { path: 'registro', component: RegistroComponent } // ruta para registro
 ];
